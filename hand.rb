4c3654a27135ac5c3ed604f2d1e3c6270ae410bc
EC2 Default User <ec2-user@ip-172-31-36-15.ec2.internal>
2021-09-06 13:27:53 +0000

    janken

--- hand.rb
+++ hand.rb
@@ -1,75 +1,69 @@
-  puts "じゃんけん..."
-def janken
-  puts "0(グー)1(チョキ)2(パー)3(戦わない)"
-  player_hand = gets.to_i
-  program_hand = rand(2)
-  jankens = ["グーを出しました","チョキを出しました","パーを出しました"]
-  puts "ホイ！"
-  puts "--------------"
-  puts "あなた:#{jankens[player_hand]}\n相手:#{jankens[program_hand]}"
-  
-   win = (player_hand == 0 && program_hand == 1) || (player_hand == 1 && program_hand == 2) || (player_hand == 2 && program_hand == 0)
-   lose = !(win)
-   draw = player_hand == program_hand
-  if draw
-    puts "あいこで..."
-    @result = "draw"
-    return true
-  elsif win
-    puts "あなたの勝ちです"
-    @result = "win"
-    return false
-  elsif player_hand == 3
-    puts "あなたはじゃんけんをしませんでした"
-    exit
-  elsif lose
-    puts "あなたの負けです"
-    @result = "lose"
-    return false
+  def janken()
+    puts "じゃんけん..."
+    puts "0(グー)1(チョキ)2(パー)3(戦わない)"
+    player_hand = gets.to_i
+    program_hand = rand(2)
+    jankens = ["グーを出しました","チョキを出しました","パーを出しました"]
+    puts "ホイ！"
+    puts "--------------"
+    puts "あなた:#{jankens[player_hand]}\n相手:#{jankens[program_hand]}"
+    
+     win = (player_hand == 0 && program_hand == 1) || (player_hand == 1 && program_hand == 2) || (player_hand == 2 && program_hand == 0)
+     lose = !(win)
+     draw = player_hand == program_hand
+    if draw
+      puts "あいこで..."
+      @result = "draw"
+      return true
+    elsif win
+      puts "あなたの勝ちです"
+      @result = "win"
+      return false
+    elsif player_hand == 3
+      puts "あなたは戦いをやめた"
+      exit
+    elsif lose
+      puts "あなたの負けです"
+      @result = "lose"
+      return false
+    end
   end
-end
-  next_game = true
-  while next_game do
-    next_game = janken
-  end
-  
-def direction
-  if  @result == "win"
-  puts "--------------"
-  puts "あっち向いて〜"
-  puts "0(上)1(下)2(左)3(右)"
-  player_finger = gets.to_i
-  program_face = rand(3)
-  direction = ["上","下","左","右"]
-  puts "ホイ！"
-  puts "--------------"
-  puts "あなた:#{direction[player_finger]}\n相手:#{direction[program_face]}"
-  if player_finger == program_face
-    puts "あなたの勝利"
-  elsif player_finger != program_face
-    puts "やり直し"
-    return true
-  end
-  end
-  if @result == "lose"
-  puts "--------------"
-  puts "あっち向いて〜"
-  puts "0(上)1(下)2(左)3(右)"
-  player_face = gets.to_i
-  program_finger = rand(3)
-  direction = ["上","下","左","右"]
-  puts "ホイ！"
-  puts "--------------"
-  puts "あなた:#{direction[player_face]}\n相手:#{direction[program_finger]}"
-  if player_face == program_finger
-    puts "あなたの敗北"
-  elsif player_face != program_finger
-    puts "やり直し"
-    return true
-  end
-  end
-end
-  next_game = true
-  while next_game do
-    next_game = direction
-  end
+    next_game = true
+    while next_game do
+      next_game = janken
+    end
+    
+    if  @result == "win"
+    puts "--------------"
+    puts "あっち向いて〜"
+    puts "0(上)1(下)2(左)3(右)"
+    player_finger = gets.to_i
+    program_face = rand(3)
+    direction = ["上","下","左","右"]
+    puts "ホイ！"
+    puts "--------------"
+    puts "あなた:#{direction[player_finger]}\n相手:#{direction[program_face]}"
+    if player_finger == program_face
+      puts "あなたの勝利"
+    elsif player_finger != program_face
+      puts "やり直し"
+      janken
+    end
+    end
+    if @result == "lose"
+    puts "--------------"
+    puts "あっち向いて〜"
+    puts "0(上)1(下)2(左)3(右)"
+    player_face = gets.to_i
+    program_finger = rand(3)
+    direction = ["上","下","左","右"]
+    puts "ホイ！"
+    puts "--------------"
+    puts "あなた:#{direction[player_face]}\n相手:#{direction[program_finger]}"
+    if player_face == program_finger
+      puts "あなたの敗北"
+    elsif player_face != program_finger
+      puts "やり直し"
+      janken
+    end
+    end
